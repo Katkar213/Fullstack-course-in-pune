d77d361eb8e554bc50183675aa96a5adc1d77da5
Katkar213 <blackship213@gmail.com>
2024-06-26 17:01:35 +0530

    just

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let arr = [
+    "Diploma in Full Stack Development With University Certificate",
+    "Corporate Training",
+    "On Job Training With Live Projects",
+    "100% Placement with submission of live project"
+  ];
+  let i = 0;
+  setInterval(function() {
+    document.getElementById("detail-box").innerText = arr[i];
+    i = (i + 1) % arr.length;
+  }, 2000);
+
+  
+// navbar color changing....
+
+document.addEventListener("scroll",()=>{
+  const header=document.getElementsByClassName("header_section");
+  const list=document.getElementsByClassName("navbar-nav");
+  
+  if(window.scrollY>0){
+    header[0].classList.add("scrolled");
+    list[0].classList.add("list-scroll")
+  }
+  else{
+    header[0].classList.remove("scrolled");
+    list[0].classList.remove("list-scroll");
+  }
+  })
+
+
+
