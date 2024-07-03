5ecc968efc1b259f182b9e8658afe6c67395058f
Katkar213 <blackship213@gmail.com>
2024-07-03 10:40:10 +0530

    all

--- js/script.js
+++ js/script.js
@@ -32,3 +32,45 @@ document.addEventListener("scroll",()=>{
 
 
 
+  // slick slider...................
+  $(document).ready(function(){
+    $('.customer-logos').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 4,
+      autoplay:true,
+      autoplaySpeed:2000,
+      infinite: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+  
+
+
+
